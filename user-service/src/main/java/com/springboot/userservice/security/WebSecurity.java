52333ada8c0bb546ab5da3b419e022da1f60a8d6
erhan.canlica <erhan.canlic@ozgur.yazilim.com.tr>
2021-08-15 02:35:35 +0300

    adding support to return json or xml

--- user-service/src/main/java/com/springboot/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/springboot/userservice/security/WebSecurity.java
@@ -1,6 +1,7 @@
 package com.springboot.userservice.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,11 +10,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private final Environment environment;
+
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http.authorizeRequests().antMatchers("/users/**")
-                .permitAll();
+                .hasIpAddress(environment.getProperty("gateway.ip"));
     }
 }
