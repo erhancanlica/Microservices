33232795860666a5b5083001dd1b673b9699aabc
erhan.canlica <erhan.canlic@ozgur.yazilim.com.tr>
2021-08-14 23:50:54 +0300

    up more user service

--- user-service/src/main/java/com/springboot/userservice/controller/UserController.java
+++ user-service/src/main/java/com/springboot/userservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.springboot.userservice.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    private final Environment environment;
+
+    public UserController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/status/check")
     public String status() {
-        return "working";
+        return "Working on port " + environment.getProperty("local.server.port");
     }
 }
